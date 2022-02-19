c6de60779e2d445622d9fd50ed1983b981c1eb9e
laughingclouds <zenithtaiga@gmail.com>
2022-02-19 22:02:29 +0530

    fine tuned the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,14 +16,26 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -38,12 +52,25 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     results
 }
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "smart";
         let contents = concat!(
             "Hemant:\n",
@@ -51,7 +78,22 @@ mod tests {
             "Pick three."
         );
 
-        assert_eq!(vec!["handsome, smart, batman"], search(query, contents))
+        assert_eq!(vec!["handsome, smart, batman"], search(query, contents));
+    }
 
+    #[test]
+    fn case_insensitive() {
+        let query = "hEmAnT";
+        let contents = concat!(
+            "Hemant:\n",
+            "handsome, smart, batman\n",
+            "Pick three.\n",
+            "Worship me."
+        );
+
+        assert_eq!(
+            vec!["Hemant:"],
+            search_case_insensitive(query, contents)
+        );
     }
 }
